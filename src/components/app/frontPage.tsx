12a155c9458cf627ce946c96d29d819747bb8a15
Johannes Brodwall <jhannes@gmail.com>
2025-07-31 18:07:10 +0200

    update front page text

--- src/components/app/frontPage.tsx
+++ src/components/app/frontPage.tsx
@@ -11,7 +11,7 @@ export function FrontPage() {
       <h1>Chill, baby! Chill!</h1>
       <div className="welcome">
         <Dialog open={open} onClose={() => navigate("/map")}>
-          <h1>Er du mer klimaflink enn Arbeiderpartiet?</h1>
+          <h1>Kan du hjelpe Arbeiderpartiet?</h1>
 
           <p>
             Visste du at produksjon av olje og gass står for{" "}
@@ -23,18 +23,30 @@ export function FrontPage() {
               en fjerdedel av Norges utslipp av klimagasser
             </a>
             ? Fra 2025 til 2040 vil dette etter Sokkeldirektorats beregninger
-            stå for 10 zillioner tonn med CO2. (TODO: kvalitetssikre tallet). Og
-            da regner vi ikke med utslippene når olja og gassen brennes i
-            utlandet.
+            stå for 10 zillioner tonn med CO2, samtidig som det planlegges å
+            bruke XX TWh med strøm årlig (gjennomsnitt) på å elektrifisere med
+            kraft fra land. (TODO: kvalitetssikre tallet). Og da regner vi ikke
+            med utslippene når olja og gassen brennes i utlandet som er cirka 50
+            ganger så mye.
           </p>
 
           <p>
-            MDG har <a href="https://mdg.no/oljeplan">en plan</a> for å gradvis
-            avvikle den mest forurensende produksjonen frem til 2040, samtidig
-            som Norge beholder en stor andel av inntektene.
+            Samtidig trenger norsk næringsliv flere ben å stå på, for sokkelen
+            begynner å tømmes for reserver. Klimautvalget 2050 har anbefalt
+            Norge å lage en "strategi for sluttfasen av norsk
+            petroleumsvirksomhet", og{" "}
+            <a href="https://mdg.no/oljeplan">MDG har laget en slik plan</a>. Vi
+            vil fase ut feltene på norsk sokkel innen 2040, og ønsker å starte
+            med feltene med høyest utslipp og lavest produksjon.
           </p>
 
-          <p>Vil du prøve å se om du klarer å lage en like god plan?</p>
+          <p>
+            Du har fått jobben som energiminister for en dag, og er bedt om å
+            lage en utfasingsplan som balanserer utslippskutt og
+            produksjonsnivåer på en best mulig måte frem mot 2040.
+          </p>
+
+          <p>Har du det som skal til for å lage en slik plan?</p>
 
           <p>
             <button onClick={() => setOpen(false)}>Jeg er klar</button>
