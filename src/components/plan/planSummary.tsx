0dde72cc7113eeb5b2af03a80038ee4a7d63b8a5
erlend <wildifyme@gmail.com>
2025-10-23 17:07:39 +0200

    start adding baseline results to plan

--- src/components/plan/planSummary.tsx
+++ src/components/plan/planSummary.tsx
@@ -7,6 +7,8 @@ import { EmissionForAllFieldsPage } from "../emissions/emissionsForAllFieldsPage
 import { EmissionSummaryPage } from "../emissions/emissionSummaryPage";
 import { ProductionSummaryPage } from "../production/productionSummaryPage";
 import { useNavigate, useLocation } from "react-router-dom";
+import { ProductionSummaryCard } from "../production/productionSummaryCard";
+import { gameData, oilEquivalentToBarrel, sumOverYears, totalProduction } from "../../data/gameData";
 
 /**
  * Displays a summary of the user's plan, including charts for
@@ -17,9 +19,20 @@ export function PlanSummary() {
   const navigate = useNavigate();
   const location = useLocation();
 
+  // Emission summary data
+  const years = gameData.gameYears;
+  const baselineEm = sumOverYears(totalProduction({}, years), "emission");
+  const resultEm = sumOverYears(totalProduction(phaseOut, years), "emission");
+  const reductionEm = Math.round(((baselineEm - resultEm) / baselineEm) * 100);
+
+  // Production summary data
+  const baselinePr = sumOverYears(totalProduction({}, gameData.gameYears), "totalProduction",);
+  const resultPr = sumOverYears(totalProduction(phaseOut, gameData.gameYears), "totalProduction",);
+  const reductionPr = Math.round(((baselinePr - resultPr) / baselinePr) * 100);
+
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "2rem", padding: "2rem", }}>
-      <div style={{ position: "absolute", placeSelf: "end",}}>
+      <div style={{ position: "absolute", placeSelf: "end", }}>
         <button
           onClick={() => navigate("/map", { state: { from: location } })}
           title="Tilbake"
@@ -27,18 +40,32 @@ export function PlanSummary() {
           X
         </button>
       </div>
-      
+
       <h2>
         Din plan
       </h2>
 
+      <div>
+        <div>
+          {Math.round(resultEm / 1_000_000)}
+          &nbsp;millioner&nbsp;tonn&nbsp;CO₂{" "}
+          <span title={baselineEm.toString()}> (-{reductionEm}%)</span>
+        </div>
+      <div>
+        {Math.round(((resultPr * oilEquivalentToBarrel) / 1_000) * 10) / 10}&nbsp;milliarder&nbsp;fat{" "}
+        <span title={baselinePr.toFixed(2)}>(-{reductionPr}%)</span>
+      </div>
+        {/* <EmissionSummaryCard />
+        <ProductionSummaryCard /> */}
+      </div>
+
       <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
 
         <div className={"charts"} style={{ gap: "2rem", }}>
-          <div style={{ border: "1px solid rgba(255,255,255,0.0)", borderRadius: "0.5rem", padding: "1rem"}}>
+          <div style={{ border: "1px solid rgba(255,255,255,0.0)", borderRadius: "0.5rem", padding: "1rem" }}>
             <ProductionReductionChart phaseOut={phaseOut} />
           </div>
-          <div style={{ border: "1px solid rgba(255,255,255,0.0)", borderRadius: "0.5rem", padding: "1rem"}}>
+          <div style={{ border: "1px solid rgba(255,255,255,0.0)", borderRadius: "0.5rem", padding: "1rem" }}>
             <EmissionStackedBarChart phaseOut={phaseOut} />
           </div>
         </div>
